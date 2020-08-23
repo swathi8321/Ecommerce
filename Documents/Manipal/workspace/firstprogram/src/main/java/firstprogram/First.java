db61dae194447b0d9f81ad4e50cfd61fc93ab878
Swathi <swathirao18@yahoo.co.in>
2020-08-23 11:42:14 +0530

    second commit

--- Documents/Manipal/workspace/firstprogram/src/main/java/firstprogram/First.java
+++ Documents/Manipal/workspace/firstprogram/src/main/java/firstprogram/First.java
@@ -5,6 +5,7 @@ public class First {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My first Program");
+		System.out.println("My second Program");
 	}
 
 }
